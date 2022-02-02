f5a3e8ba6938dbc5bcf3d1ab4aa0b5606b9ebeaa
inoxidables <ferreteriainoxidables@gmail.com>
2022-02-02 10:59:46 -0500

    encogi el textarea

--- contacto/forms.py
+++ contacto/forms.py
@@ -8,4 +8,4 @@ class FormularioContacto(forms.Form):
 
      email=forms.CharField(label="email" , required=True)
 
-     contenido=forms.CharField(label="contenido" , widget=forms.Textarea)
+     contenido=forms.CharField(label="contenido" , widget=forms.Textarea(attrs={'rows': 8, 'cols': 25}))
